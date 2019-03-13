e6e0f01f88ec8440cff49febf9dc2bf1d7810821
Madison Cooney <madison.l.cooney@gmail.com>
2019-03-14 10:14:09 +1100

    boop

---
+++ prac2.js
@@ -0,0 +1,12 @@
+//Question 1: find roots of quadratic equation with general formula ax^2+bx+c
+function quadraticRoots() {
+
+  var yeet = "kill me";
+  // hello worlddd
+
+  thank you very cool
+  go green
+  return "we've pretty much finished this prac";
+};
+
+//Question 2:
