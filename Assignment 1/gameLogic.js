d6b0321f40e83dd3ef1fd47bb08b9a6bd7c7bbf8
James Sandison <jsan0015@student.monash.edu>
2019-03-25 17:12:38 +1100

    my code - james

--- Assignment 1/gameLogic.js
+++ Assignment 1/gameLogic.js
@@ -13,9 +13,64 @@
  *    "yellow"
  *     "red"
 */
+let userInputSequence = [];
+let failedLastSequence = false;
+let sequenceLength = 4;
+let sequence;
+let correctSequencesAtCurrent = 0;
+
 function buttonSelected(whichButton)
 {
     // Include your own code here
+    userInputSequence.push(whichButton);
+    updateDisplay();
+    if (userInputSequence.length === sequenceLength)
+    {
+      console.log(userInputSequence);
+      for (let j = 0; j < sequenceLength; j++)
+      {
+        if (userInputSequence[j] !== sequence[j])
+        {
+          showFailure();
+          sequenceProgress('failed');
+
+          //
+
+          return runGameWithWait();;
+        }
+      }
+      showSuccess();
+      sequenceProgress('success');
+      return runGameWithWait();
+    }
+
+}
+
+function sequenceProgress(result) {
+    if (result === 'failed') {
+      if (failedLastSequence === true) {
+        sequenceLength = 4;
+        correctSequencesAtCurrent = 0;
+      }
+      else if (sequenceLength > 4) {
+        sequenceLength--;
+        correctSequencesAtCurrent = 0;
+      }
+      else {
+        sequenceLength = 4;
+        correctSequencesAtCurrent = 0;
+      }
+    }
+    else {
+      if (correctSequencesAtCurrent + 1 === sequenceLength - 2) {
+        sequenceLength++;
+        correctSequencesAtCurrent = 0;
+      }
+      else {
+        correctSequencesAtCurrent++;
+      }
+    }
+    return
 }
 
 /*
@@ -27,12 +82,23 @@ function buttonSelected(whichButton)
  *    "yellow"
  *    "red"
 */
+
 function giveNextSequence()
 {
-    // Include your own code here
+    let colours = ["blue","green","yellow","red"];
+    let colourIndex;
+    sequence = [];
+    for (let i = 0; i < sequenceLength; i++)
+    {
+      colourIndex = Math.ceil(Math.random()*4) - 1;
+      sequence.push(colours[colourIndex]);
+    }
 
     // Example return statement.
-    return ["blue","blue","blue"];
+    console.log(sequence);
+    userInputSequence = [];
+    updateDisplay();
+    return sequence;
 }
 
 
@@ -42,6 +108,8 @@ function giveNextSequence()
 */
 function sequenceHasDisplayed()
 {
+    updateDisplay();
+    displayToastMessage("Enter the sequence.");
     // Include your own code here
 }
 
@@ -62,6 +130,8 @@ function sequenceHasDisplayed()
 function userChoiceTimeout()
 {
     // Include your own code here
+    showFailure();
+    disallowButtonPresses();
 }
 
 /*
@@ -78,3 +148,17 @@ function changeMode(mode)
 }
 
 // You may need to write toher functions.
+function updateDisplay() {
+  let outputRefArea = document.getElementById("output");
+  outputRefArea.innerHTML = "Button Presses Remaining: <strong>" + buttonPressesRemaining + "<strong/><br/>";
+  outputRefArea.innerHTML += "Current Sequence Length: <strong>" + sequenceLength + "<strong/><br/>";
+  outputRefArea.innerHTML += "Correct Sequences at Current Level: <strong>" + correctSequencesAtCurrent + "<strong/><br/>";
+  outputRefArea.innerHTML += "Sequences from next level: <strong>" + (sequenceLength - correctSequencesAtCurrent - 2) + "<strong/><br/><br/>";
+  if (playingState === 2) {
+    outputRefArea.innerHTML += "<strong><h5>Watch Current Sequence!<strong/><h5/>";
+  } else if (playingState === 3) {
+    outputRefArea.innerHTML += "<strong><h5>Enter the Sequence!<strong/><h5/>"
+  } else {
+    outputRefArea.innerHTML += "<strong><h5>Start a New Game!<strong/><h5/>"
+  }
+}
