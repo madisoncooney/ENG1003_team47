4093bcfb70f8d385ec0fc619c1eee75bec4ded34
James Sandison <jsan0015@student.monash.edu>
2019-04-04 16:48:48 +1100

    update
    commenting and testing to be done

--- Assignment Absolute Test/gameLogic.js
+++ Assignment Absolute Test/gameLogic.js
@@ -1,74 +1,139 @@
 "use strict";
 
-//Declare and define some variables...
-//Let 'outputRefArea' be the output box on the app
+// Declare and define required global variables
+let userInputSequence = [];
+let failedLastSequence = false;
+let sequenceLength = 4;
+let sequence;
+let correctSequencesAtCurrent = 0;
+let tiltSelected;
+
+
+// Let 'outputRefArea' be the output box on the app
+// Format the initial screen to give instructions
 let outputRefArea = document.getElementById("output");
 outputRefArea.style.width = "90%";
 outputRefArea.style["padding"] = "5%";
 outputRefArea.style["text-align"] = 'center';
-let userInputSequence = [];
-let failedLastSequence = false;
-let timeBetweenSequences = 1000;
-let sequenceLength = 4;
-let sequence;
-let correctSequencesAtCurrent = 0;
-let beta, gamma, quat0, quat1, quat2, quat3, tiltSelected;
-let degreeThreshold = 7.5;
+outputRefArea.innerHTML = "<h3 style='margin:8px'>Welcome to Simon</h3><h5 style='margin:8px'>Press the play button to begin!</h5>";
+
+
 
 
 ///////////////////////////////////////////////////
 // Code from SensorTest App
 ///////////////////////////////////////////////////
 
+/*
+ *
+ * Sensor Test web app
+ *
+ * Copyright (c) 2019  Monash University
+ *
+ * Written by Machael Wybrow and Arvind Kaur
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+*/
+
 // Start: code for device orientation
-outputRefArea.innerHTML = "<h3 style='margin:8px'>Welcome to Simon</h3><h5 style='margin:8px'>Press the play button to begin!</h5>";
-let deviceAbsolute = null;
-// try-catch: exception handling
+ let deviceAbsolute = null;
+ // try-catch: exception handling
+ try
+ {
+		 // initialising object for device orientation
+		 deviceAbsolute = new AbsoluteOrientationSensor({ frequency: 10 });
 
-// initialising object for device orientation
-deviceAbsolute = new AbsoluteOrientationSensor({ frequency: 10 });
+		 //if sensor is available but there is problem in using it
+		 deviceAbsolute.addEventListener('error', event => {
+		 // Handle runtime errors.
+		 if (event.error.name === 'NotAllowedError')
+		 {
+			 outputRefArea.innerHTML += "Permission to access sensor was denied.";
+		 }
+		 else if (event.error.name === 'NotReadableError' )
+		 {
+			 outputRefArea.innerHTML += "Cannot connect to the sensor.";
+		 }});
+		 // when sensor has a reading, call the function
+		 deviceAbsolute.addEventListener('reading', () => absoluteOrientationListener(deviceAbsolute));
 
-//if sensor is available but there is problem in using it
-deviceAbsolute.addEventListener('error', event => {
-	// Handle runtime errors.
-	if (event.error.name === 'NotAllowedError')
-	{
-		outputRefArea.innerHTML += "<p>Permission to access sensor was denied.<p/>";
-	}
-	else if (event.error.name === 'NotReadableError' )
-	{
-		outputRefArea.innerHTML += "<p>Cannot connect to the sensor.<p/>";
-	}});
-
-// when sensor has a reading, call the function
-deviceAbsolute.addEventListener('reading', () => absoluteOrientationListener(deviceAbsolute));
-
-//start the sensor
-deviceAbsolute.start();
+		 //start the sensor
+		 deviceAbsolute.start();
+ }
+ catch (error)
+ {
+ // Handle construction errors.
+	 let errorText = "";
+	 if (error.name === 'SecurityError')
+	 {
+		 errorText = "Sensor construction was blocked by the Feature Policy.";
+	 }
+	 else if (error.name === 'ReferenceError')
+	 {
+		 errorText =" Sensor is not supported by the User Agent.";
+	 }
+	 else
+	 {
+		 errorText = "Sensor not supported";
+	 }
+	 console.log(errorText);
+ }
 
 ////////////////////////////////////////
 // End code from sensor test App
 ////////////////////////////////////////
 
 
-//Function fires whenever the deviceAbsolute event listener gets new data and calculates data
+
+
+
+
+// Function fires whenever the deviceAbsolute event listener
+// recieves new data from the sensor.
+// function converts quaterions to beta and gamma
 function absoluteOrientationListener(event) {
 	let radianToDegrees = 180/Math.PI;
 
-	//normalize quaternions to ensure expected results in calculations (as from: https://www.cprogramming.com/tutorial/3d/quaternions.html)
-	let magnitude = Math.sqrt((event.quaternion[0]**2) + (event.quaternion[1]**2) + (event.quaternion[2]**2) + (event.quaternion[3]**2));
-	quat0 = event.quaternion[0]/magnitude;
-	quat1 = event.quaternion[1]/magnitude;
-	quat2 = event.quaternion[2]/magnitude;
-	quat3 = event.quaternion[3]/magnitude;
 
-	// use DJI algorithm to convert quaternions to euler angles relative to device orientation (https://stackoverflow.com/questions/5782658/extracting-yaw-from-a-quaternion)
-	let roll = Math.atan2(2 * (quat3*quat2 + quat0*quat1),1 - 2*(quat1*quat1 + quat2*quat2));
+	// normalize quaternions to ensure expected results in quaternion
+	// (as from: https://www.cprogramming.com/tutorial/3d/quaternions.html)
+	let magnitude = Math.sqrt((event.quaternion[0]**2)
+													+ (event.quaternion[1]**2)
+													+ (event.quaternion[2]**2)
+													+ (event.quaternion[3]**2));
+
+	let quat0 = event.quaternion[0]/magnitude;
+	let quat1 = event.quaternion[1]/magnitude;
+	let quat2 = event.quaternion[2]/magnitude;
+	let quat3 = event.quaternion[3]/magnitude;
+
+
+	// use DJI algorithm to convert quaternions to euler angles relative to
+	// device orientation. Found derivation of equations here:
+	// (https://stackoverflow.com/questions/5782658/extracting-yaw-from-a-quaternion)
 	let pitch = Math.asin(2 * (quat2*quat0 - quat3 * quat1));
 	let yaw = Math.atan2(2 * (quat3*quat0 + quat1*quat2), -1 + 2*(quat0*quat0 + quat1*quat1));
 
-	beta = checkValue(radianToDegrees*pitch); // devices took pitch as the beta angle
-	gamma = checkValue(radianToDegrees*yaw); // devices took yaw as beta angle
+	let beta = checkValue(radianToDegrees*pitch); // devices took pitch as the beta angle
+	let gamma = checkValue(radianToDegrees*yaw); // devices took yaw as beta angle
 
 	// function to convert all values to lie within -90 -> 90 degrees
 	function checkValue(value) {
@@ -84,12 +149,13 @@ function absoluteOrientationListener(event) {
 	}
 
 	// call the tilt selection function to use gamma and beta to determine orientation
-	tiltSelection();
+	tiltSelection(beta, gamma);
 }
 
 
-function tiltSelection()
+function tiltSelection(beta, gamma)
 {
+	let degreeThreshold = 7.5;
 	// check if a valid situation for tilt selection to occur
 	if (controlMode === TILT_MODE && usersTurn === true)
 	{
@@ -181,11 +247,12 @@ function tiltButtonSelect(feedbackButton)
 function buttonSelected(whichButton)
 {
 	// Include your own code here
+	let timeBetweenSequences = 1000;
+
 	userInputSequence.push(whichButton);
-	updateDisplay();
+	updateDisplay(STATE_USER);
 	if (userInputSequence.length === sequenceLength)
 	{
-		console.log(userInputSequence);
 		for (let j = 0; j < sequenceLength; j++)
 		{
 			if (userInputSequence[j] !== sequence[j])
@@ -200,6 +267,7 @@ function buttonSelected(whichButton)
 		}
 		showSuccess();
 		sequenceProgress('success');
+		updateDisplay(STATE_WATCH);
 		setTimeout(runGame,timeBetweenSequences);
 		return;
 	}
@@ -247,7 +315,7 @@ function sequenceProgress(result) {
 */
 function giveNextSequence()
 {
-	updateDisplay();
+	updateDisplay(STATE_WATCH);
 	let colours = ["blue","green","yellow","red"];
 	let colourIndex;
 	sequence = [];
@@ -257,9 +325,9 @@ function giveNextSequence()
 		sequence.push(colours[colourIndex]);
 	}
 
-	// return statemen
-	console.log(sequence); //log
+	// return statement
 	userInputSequence = [];
+	updateDisplay(STATE_WATCH);
 	return sequence;
 }
 
@@ -270,7 +338,7 @@ function giveNextSequence()
 */
 function sequenceHasDisplayed()
 {
-	updateDisplay(); //Update game information to players
+	updateDisplay(STATE_USER); //Update game information to players
 	displayToastMessage("Enter the sequence."); //Prompt the user to play
 	if (controlMode === TOUCH_MODE)
 	{
@@ -303,7 +371,8 @@ function userChoiceTimeout()
 			sequenceProgress('failed');
 			sequenceProgress('failed');
 			enterWaitState();
-			return updateDisplay();
+			usersTurn = false;
+			return updateDisplay(STATE_WAIT);
 		}
 
 		// Select the stored selected value and then change it back to undefined.
@@ -326,7 +395,7 @@ function userChoiceTimeout()
 		sequenceProgress('failed');
 		sequenceProgress('failed');
 		disallowButtonPresses();
-		updateDisplay();
+		updateDisplay(STATE_WAIT);
 	}
 	return;
 }
@@ -361,28 +430,28 @@ function changeMode(mode)
 }
 
 //Handles passing game information to the player
-function updateDisplay() {
+function updateDisplay(passedState) {
 	outputRefArea.style["text-align"] = 'justify';
 	//Change 'outputRefArea' to display helpful information
-	outputRefArea.innerHTML = "Button Presses Remaining: <strong>" + buttonPressesRemaining + "<strong/><br/>";
-	outputRefArea.innerHTML += "Current Sequence Length: <strong>" + sequenceLength + "<strong/><br/>";
-	outputRefArea.innerHTML += "Correct Sequences at Current Level: <strong>" + correctSequencesAtCurrent + "<strong/><br/>";
-	outputRefArea.innerHTML += "Sequences from next level: <strong>" + (sequenceLength - correctSequencesAtCurrent - 2) + "<strong/><br/>";
+	outputRefArea.innerHTML = "Button Presses Remaining: <strong>" + buttonPressesRemaining + "</strong><br/>";
+	outputRefArea.innerHTML += "Current Sequence Length: <strong>" + sequenceLength + "</strong><br/>";
+	outputRefArea.innerHTML += "Correct Sequences at Current Level: <strong>" + correctSequencesAtCurrent + "</strong><br/>";
+	outputRefArea.innerHTML += "Sequences from next level: <strong>" + (sequenceLength - correctSequencesAtCurrent - 2) + "</strong>";
 
 
 	//Direct the player to which state they are currently in
-	if (playingState === 2) //Sequence is about to display
+	if (passedState === STATE_WATCH) //Sequence is about to display
 	{
-		outputRefArea.innerHTML += "<strong><h5>Watch Current Sequence!<strong/><h5/>";
+		outputRefArea.innerHTML += "<strong><h5>Watch Current Sequence!</strong></h5>";
 	}
 
-	else if (playingState === 3) //Player has watched the sequence
+	else if (passedState === STATE_USER) //Player has watched the sequence
 	{
-		outputRefArea.innerHTML += "<strong><h5>Enter the Sequence!<strong/><h5/>";
+		outputRefArea.innerHTML += "<strong><h5>Enter the Sequence!</strong><h5/>";
 	}
 
-	else if (playingState === 1)//Player has failed or not begun yet
+	else if (passedState === STATE_WAIT)//Player has failed or not begun yet
 	{
-		outputRefArea.innerHTML += "<strong><h5>Start a New Game!<strong/><h5/>";
+		outputRefArea.innerHTML += "<strong><h5>Start a New Game!</strong><h5/>";
 	}
 }
